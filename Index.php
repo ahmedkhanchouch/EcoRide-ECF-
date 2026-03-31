a259deca6fdf33d92393c43d67f11d2e895b8b0c
Khanchouch Ahmed <145472248+ahmedkhanchouch@users.noreply.github.com>
2026-03-31 21:41:26 +0200

    ajout de page index.php et header.html

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include "HTML/header.html";
+    function sayHi()
+    {
+        return "Hi";
+    }
+    ?>
+
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
